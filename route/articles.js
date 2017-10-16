ef5521840349c71cfe403adb4c7dc0873b8738d4
Debanshu <lalwanidevanshu5@gmail.com>
2017-10-17 02:01:08 +0530

    Added Authentication with passport and added access control

--- route/articles.js
+++ route/articles.js
@@ -1,8 +1,9 @@
 const express=require('express');
 const router=express.Router();
 const {Article}=require('../models/article');
+const {User}=require('../models/user');
 
-router.get('/add',(req,res)=>{
+router.get('/add',ensureAuthenticated,(req,res)=>{
   res.render('add',{
     title:"Add Article"
   });
@@ -10,24 +11,43 @@ router.get('/add',(req,res)=>{
 
 router.get('/:id',(req,res)=>{
   Article.findById(req.params.id).then((article)=>{
-    res.render('article',{
-      article:article
-    });
+    User.findById(article.author).then((user)=>{
+      res.render('article',{
+        article:article,
+        author:user.name
+      });
+    }).catch((e)=>console.log(e));
   }).catch((e)=>console.log(e));
 });
 
-router.get('/edit/:id',(req,res)=>{
+function ensureAuthenticated(req,res,next){
+  if(req.isAuthenticated()){
+    return next();
+  }
+  else{
+    req.flash('danger','Please Login');
+    res.redirect('/users/login');
+  }
+};
+
+router.get('/edit/:id',ensureAuthenticated,(req,res)=>{
   Article.findById(req.params.id).then((article)=>{
+    if(article.author!=req.user._id){
+      req.flash('danger','Not Authorized');
+      res.redirect('/');
+    }
+    else {
     res.render('edit',{
       title:'Edit Article',
       article:article
     });
+  }
   }).catch((e)=>console.log(e));
 });
 
 router.post('/add',(req,res)=>{
   req.checkBody('title','Title is required').notEmpty();
-  req.checkBody('author','Author is required').notEmpty();
+  //req.checkBody('author','Author is required').notEmpty();
   req.checkBody('body','Body is required').notEmpty();
 
   let err=req.validationErrors();
@@ -40,7 +60,7 @@ router.post('/add',(req,res)=>{
   else{
       var article=new Article({
       title:req.body.title,
-      author:req.body.author,
+      author:req.user._id,
       body:req.body.body
     });
 
@@ -66,10 +86,23 @@ router.post('/edit/:id',(req,res)=>{
 });
 
 router.delete('/:id',(req,res)=>{
+  if(!req.user._id){
+    res.status(500).send();
+  }
+  else{
   let query={_id:req.params.id};
-  Article.remove(query).then(()=>{
-    res.send('Success');
-  }).catch((e)=>console.log(e));
+
+  Article.findById(req.params.id,function(err,article){
+    if(article.author!=req.user._id){
+      res.status(500).send();
+    }
+    else{
+      Article.remove(query).then(()=>{
+        res.send('Success');
+      }).catch((e)=>console.log(e));
+    }
+  });
+}
 });
 
 module.exports=router;
